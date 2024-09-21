8d0cccb55bbd2509ad689f9ef963e686f0c3751e
Andrew Montanus <amontanus@wcpss.net>
2024-09-21 16:15:49 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -7,13 +7,3 @@ def key():
 
 def test_1(key):
     assert key
-
-def test_2(key):
-    assert isinstance(key, tuple)
-
-def test_3(key):
-    assert len(key) == 3
-
-def test_4(key):
-    assert key == (2, 4, 8)
-
